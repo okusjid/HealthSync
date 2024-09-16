cce068f14b83e813aab378eef98030bcdffbde42
okusjid <l200927@lhr.nu.edu.pk>
2024-09-16 19:48:57 +0500

    [refactor] users views

--- src/apps/users/views.py
+++ src/apps/users/views.py
@@ -1,12 +1,7 @@
 from rest_framework import generics, permissions
-from rest_framework.response import Response
-from rest_framework_simplejwt.tokens import RefreshToken
 from .models import User
-from .models import Doctor, Patient
-from .serializers import RegisterSerializer, UserSerializer, DoctorSerializer, PatientSerializer
+from .serializers import RegisterSerializer, UserSerializer
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.views import APIView
-from django.http import Http404
 
 
 # User Registration View
